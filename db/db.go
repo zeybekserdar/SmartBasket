b07f1af9bd0af1d72776e74f3a41985005c03aaa
Serdar Zeybek <serdarzeybek@outlook.com>
2022-03-10 00:49:04 +0300

    db settings read .env file

--- db/db.go
+++ db/db.go
@@ -1,10 +1,19 @@
 package db
 
 import (
+	"SmartBasket/helpers"
+	"fmt"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
-func Connect() (*gorm.DB,error){
-	return gorm.Open(postgres.Open("postgres://user:pass@localhost:5432/SmartBasket"),&gorm.Config{})
+var user = helpers.GetEnvKey("DB_USER")
+var password = helpers.GetEnvKey("DB_PASS")
+var host = helpers.GetEnvKey("DB_HOST")
+var port = helpers.GetEnvKey("DB_PORT")
+var db = helpers.GetEnvKey("DB_NAME")
+
+func Connect() (*gorm.DB, error) {
+	connectionString := fmt.Sprintf("postgres://%s:%s@%s:%s/%s", user, password, host, port, db)
+	return gorm.Open(postgres.Open(connectionString), &gorm.Config{})
 }
