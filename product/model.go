e3b66dfbc49932132550fe7ddb8073f9fde801ea
Serdar Zeybek <serdarzeybek@outlook.com>
2022-03-10 00:51:18 +0300

    change product model

--- product/model.go
+++ product/model.go
@@ -1,8 +1,23 @@
 package product
 
+import (
+	"github.com/gofrs/uuid"
+	"gorm.io/gorm"
+)
+
 type Product struct {
-	ID uint 	`json:"id"`
-	Name string	`json:"name"`
-	Price string`json:"price"`
-	Type string	`json:"type"`
+	ID         uuid.UUID `json:"id" gorm:"primary_key;uuid"`
+	Name       string    `json:"name" validate:"required"`
+	Price      string    `json:"price"`
+	Type       string    `json:"type"`
+	gorm.Model `json:"-"`
+}
+
+func (product *Product) BeforeCreate() error {
+	id, err := uuid.NewV4()
+	if err != nil {
+		return err
+	}
+	product.ID = id
+	return nil
 }
