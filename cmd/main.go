46254a7208b47257d5a24206c650de3f2771af41
Serdar Zeybek <serdarzeybek@outlook.com>
2022-03-10 00:53:54 +0300

    add user endpoints & config logger

--- cmd/main.go
+++ cmd/main.go
@@ -3,29 +3,54 @@ package main
 import (
 	"SmartBasket/db"
 	"SmartBasket/product"
+	"SmartBasket/user"
 	"fmt"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/monitor"
 	"log"
 )
 
-func main(){
-	database,err := db.Connect()
+func main() {
+	database, err := db.Connect()
 	if err != nil {
-		log.Fatalf("could not connect to db %v",err)
+		log.Fatalf("could not connect to db %v", err)
 	}
 
-	repository := product.NewRepository(database)
-	err = repository.Migration()
+	productRepository := product.NewRepository(database)
+	userRepository := user.NewRepository(database)
+	err = productRepository.Migration()
+	err = userRepository.Migration()
 	if err != nil {
 		log.Fatal(err)
 	}
-	service := product.NewService(repository)
-	handler := product.NewHandler(service)
+	productService := product.NewService(productRepository)
+	userService := user.NewService(userRepository)
+	productHandler := product.NewHandler(productService)
+
+	userHandler := user.NewUserHandler(userService)
 
 	app := fiber.New()
-	app.Get("/products/:id",handler.Get)
-	app.Post("/products/",handler.Create)
-	app.Listen(":8000")
+	app.Use(logger.New())
+
+	//app.Use(requestid.New())
+
+	app.Get("api/products/:id", productHandler.Get)
+	app.Get("api/products", productHandler.GetAll)
+	app.Delete("api/products/:id", productHandler.Delete)
+	app.Put("api/products/:id", productHandler.Update)
+	app.Post("api/products/", productHandler.Create)
+
+	app.Post("api/users/login", userHandler.Login)
+	app.Post("api/users/register", userHandler.Register)
+
+	// Monitor
+	app.Get("/monitor", monitor.New())
+
+	appErr := app.Listen(":8000")
+	if appErr != nil {
+		log.Fatalf("Application failed", appErr)
+	}
 
 	fmt.Println(database.Config)
 }
