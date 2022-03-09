fe658d4a139d073f420607c8a219fbe152d0b7b8
Serdar Zeybek <serdarzeybek@outlook.com>
2022-03-10 00:49:41 +0300

    add getAll service

--- product/service.go
+++ product/service.go
@@ -1,24 +1,43 @@
 package product
 
-type Service interface {
-	Get(id uint) (*Product,error)
-	Create(model Product) (uint,error)
+import (
+	"github.com/gofrs/uuid"
+)
+
+type IProductService interface {
+	Get(id uuid.UUID) (*Product, error)
+	GetAll() (*[]Product, error)
+	Create(model Product) (*Product, error)
+	Update(old Product, new Product) (uuid.UUID, error)
+	Delete(model Product) (uuid.UUID, error)
 }
 
 type service struct {
 	repository Repository
 }
 
-func (s service) Get(id uint) (*Product, error) {
+func (s service) GetAll() (*[]Product, error) {
+	return s.repository.GetAll()
+}
+
+func (s service) Update(old Product, new Product) (uuid.UUID, error) {
+	return s.repository.Update(old, new)
+}
+
+func (s service) Delete(model Product) (uuid.UUID, error) {
+	return s.repository.Delete(model)
+}
+
+func (s service) Get(id uuid.UUID) (*Product, error) {
 	return s.repository.Get(id)
 }
 
-func (s service) Create(product Product) (uint, error) {
+func (s service) Create(product Product) (*Product, error) {
 	return s.repository.Create(product)
 }
 
-var _ Service = service{}
+var _ IProductService = service{}
 
-func NewService(repository Repository) Service{
+func NewService(repository Repository) IProductService {
 	return service{repository: repository}
 }
